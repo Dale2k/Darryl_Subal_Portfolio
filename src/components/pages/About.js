0eb8c2104783b8c08774a4f916df5cef34e83e59
Darryl Subal <dsubal@att.net>
2023-06-12 09:22:48 -0400

    update about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,16 +13,17 @@ export default function About() {
           <Col>
             <p>
               Hi, I am Darryl Subal. I was born in a Cleveland, OH suburb and
-              currently reside in Charlotte, NC. I majored in Accounting,
-              earning a BBA from Kent State University along with a BS in Fine
-              and Professional Arts and an Associate Degree in Healthcare IT
-              Technologies from Cuyahoga Community College. In February, 2023 I
-              attended UNCC Coding Bootcamp. I have experience with Javascript,
-              HTML, CSS, Bootstrap, Node, SQL, Mongo, and React. A few of the
-              applications I have worked on include a password generator, note
-              taker, a cms style employee database, and a day planner. One of my
-              best qualities is to be open to new experiences and to know
-              learning is a continual life long process .
+              currently reside in Charlotte, NC. I earned the Full Stack Web
+              Development Certificate from UNC Charlotte in may 2023. Attending
+              Kent State University I earned a Bachelor of Business
+              Administration, major in Accounting and an Associate Degree in
+              Information Technologies from Cuyahoga Community College. I have
+              experience with Javascript, HTML, CSS, Bootstrap, Node, SQL,
+              Mongo, and React. A few of the applications I have worked on
+              include a password generator, note taker, a cms style SQL employee
+              database, and a Node.js day planner. One of my best qualities is
+              to be open to new experiences and to know learning is a continual
+              life long process .
             </p>
           </Col>
           <Col xs={6}>
